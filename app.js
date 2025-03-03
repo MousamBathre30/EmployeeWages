bb5c1c3ae53e26ab9383e35427a6b5cff754c1ca
Mousam bathre <bathremousam@gmail.com>
2025-03-03 18:55:23 +0530

    [Mousam Bathre] adding Use Case 2 for calculating wages

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 // uc 1
-
+// checking the employe present or not 
 const IS_ABSENT = 0;
 
 let empCheck = Math.floor(Math.random() * 10) % 2  ;
@@ -11,3 +11,30 @@ if(empCheck  == IS_ABSENT){
 } else{
     console.log("Employee is PRESENT");
 }
+
+// UC-2
+// assigning the variable
+const IS_PART_TIME = 1 ;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+
+empCheck = Math.floor(Math.random() * 10) % 3;
+
+// adding switch statemnt for checking the emphrs
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+    default:
+        empHrs = 0;
+        break;
+}
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage: " + empWage);
