4d0b07e7456474bf96079e549ed991c5fc4f20cd
Mousam bathre <bathremousam@gmail.com>
2025-03-03 19:18:11 +0530

    [Mousam Bathre] adding the UC_5 calculate wages on comdition hours/Month

--- app.js
+++ app.js
@@ -45,3 +45,18 @@ for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
 }
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
+
+
+// UC 5 
+const MAX_HRS_IN_MONTH = 100;
+const NUM_OF_WORKING_DAYS_ = 10;
+let totalEmpHrs = 0;
+let totalWorkingDays = 0;
+
+while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
+    let empCheck = Math.floor(Math.random() * 10) % 3 ;
+    totalEmpHrs += getWorkingHours(empCheck);
+}
+let empWage_for_Condition = totalEmpHrs * WAGE_PER_HOUR;
+console.log("UC-5 - Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wags : " + empWage);
