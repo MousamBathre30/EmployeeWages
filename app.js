056216e6ca85196f9173110d63a8719fa5b26e9d
Mousam bathre <bathremousam@gmail.com>
2025-03-03 21:08:50 +0530

    [Mousam Bathre] adding the UC 7 helper functions

--- app.js
+++ app.js
@@ -70,8 +70,68 @@ let empWage_for_Condition = totalEmpHrs * WAGE_PER_HOUR;
 console.log("UC-6 - Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Wags : " + empWage);
 
 
+let totEmpWage = 0;
+
+function sum(dailyWage){
+    totEmpWage += dailyWage;
+}
+
+empDailyWageArr.forEach(sum);
+
+console.log("UC7A - Total Days: " + totalWorkingDays + " Total Hrs: " + totalEmpHrs + " Emp Eage: "+ totEmpWage);
+
+function totalWages(totalWage , dailyWage){
+    return totalWage + dailyWage;
+}
+
+console.log("UC7A - Emp Wage with reduce : " + empDailyWageArr.reduce(totalWages , 0));
+
+let dailyCntr = 0;
+
+function mapDayWithWage(dailyWage){
+    dailyCntr ++ ;
+    return dailyCntr + " = " + dailyWage ;
+}
+
+let mapDayWageArr = empDailyWageArr.map(mapDayWithWage);
+console.log("UC7B - Daily Wage Map");
+console.log(mapDayWageArr);
+
+// UC 7C - show Days when Full time wage of 160 were earjec 
+
+function fulltimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+
+let fullDayWageArr = mapDayWageArr.filter(fulltimeWage);
+console.log("UC7c - Daily wage Filter when Fulltime Wage Earned");
+console.log(fullDayWageArr);
 
 
+// UC 7D - Find the first occurrence when Full Time Wage was earned using find function
+
+function findFullTimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+
+console.log("UC-7D - First time fulltime wage was earned on Day: " + mapDayWageArr.find(findFullTimeWage));
+
+// UC-7E - check if every eleent of full time wage is truely holding full time wage 
+
+function isAllFullTimeWage(dailyWage){
+    return dailyWage.includes("160");
+}
+
+console.log("UC 6E - Check all element have full time wage: " + fullDayWageArr.every(isAllFullTimeWage));
+
+// UC - 7G find the number of days the employee worked
+
+function totalDaysWorked(numsOFdays , dailyWage){
+    if(dailyWage > 0) return numsOFdays+1;
+    return numsOFdays;
+}
+
+console.log("UC 7G - Number of Days Emp Worked: " + empDailyWageArr.reduce(totalDaysWorked,0));
 
 
 
