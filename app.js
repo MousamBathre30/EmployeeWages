3a7dec25c937bda85846841f97cef82d2c93b846
Mousam bathre <bathremousam@gmail.com>
2025-03-03 19:09:27 +0530

    [Mousam Bathre] adding the UC_4 Calculating Wages for Month

--- app.js
+++ app.js
@@ -35,8 +35,13 @@ switch (empCheck) {
         break;
   }
 }
+
+// UC 4
+const NUM_OF_WORKING_DAYS = 20;
 let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
