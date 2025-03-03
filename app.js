118e604f3bfc881674e31cbc17740446e051c674
Mousam bathre <bathremousam@gmail.com>
2025-03-03 19:00:58 +0530

    [Mousam Bathre] merging the UC_3

--- app.js
+++ app.js
@@ -20,11 +20,10 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
 
-empCheck = Math.floor(Math.random() * 10) % 3;
 
 // adding switch statemnt for checking the emphrs
+function getWorkingHours(empCheck){
 switch (empCheck) {
     case IS_PART_TIME:
         empHrs = PART_TIME_HOURS;
@@ -34,7 +33,10 @@ switch (empCheck) {
     default:
         empHrs = 0;
         break;
+  }
 }
-
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Emp Wage: " + empWage);
