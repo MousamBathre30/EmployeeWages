93f47979b7f78d24e60dffb86a09fb69e2005917
Mousam bathre <bathremousam@gmail.com>
2025-03-03 21:26:12 +0530

    [Mousam Bathre] adding the arrow function feature

--- app.js
+++ app.js
@@ -145,8 +145,31 @@ console.log(empDailyWageMap);
 console.log("UC7A - Emp Wage Map totalHRs: " + Array.from(empDailyWageMap.values()).reduce(totalWages,0));
 
 
+// UC 9 Arrow Functions 
 
+const findTotal = (totalVal , dailyVal) => {
+    return totalVal + dailyVal;
+}
 
+let count = 0;
+let totalHours = Array.from(empDailyHrsMap.values()).reduce(findTotal,0);
+let totalSalary = empDailyWageArr.filter(dailyWage => dailyWage > 0).reduce(findTotal,0);
+
+console.log("UC 9A - Emp Wage With Arrow: " + "Total Hours: "+ totalHours + " Total WAges: " + totalSalary);
+
+let nonWorkingDays = new Array();
+let partWorkingDays = new Array();
+let fullWorkingDays = new Array();
+
+empDailyHrsMap.forEach((value,key,map) => {
+    if(value == 8) fullWorkingDays.push(key);
+    else if (value == 4) partWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+
+console.log ("Full Working Days: " + fullWorkingDays);
+console.log("Part time working days " + partWorkingDays);
+console.log("Non Working Days: " + nonWorkingDays);
 
 
 
